dmoe1975/android_device_moto_shamu

Shamu : add listen platform information file
- listen platform information is configured thru
this listen platform xml file. Listen Hal parses
this file to get the platform configuration data
- Current platform information includes listen ACDB
device IDs for CDSP and ADSP, maximum cdsp sessions,
maximum adsp sessions, maximum adsp keywords, maximum
adsp user pairs, maximum cdsp keywords, maximum cdsp
user pairs and running either on CDSP or ADSP
- if this platform file is not present, listen hal works
with the default configuration of one session with one
keyword always running on ADSP

Change-Id: I7cab3d84a04ebeac613648817511b7dee0859962

device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Listen configuration file
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/listen_platform_info.xml:system/etc/listen_platform_info.xml
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \

skippy5439812/device_motorola_shamu

device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Listen configuration file
+PRODUCT_COPY_FILES += \
+    device/motorola/shamu/listen_platform_info.xml:system/etc/listen_platform_info.xml
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \